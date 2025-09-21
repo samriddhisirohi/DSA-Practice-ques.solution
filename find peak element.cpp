84b9121d78f69f42cbf7436dacec261544287099
samriddhisirohi <samriddhisirohi28@gmail.com>
2025-09-21 23:55:49 +0530

    Create find peak element.cpp
    revise

---
+++ find peak element.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+          int start=0, end=nums.size()-1 ;
+          while(start<=end)
+          {
+            int mid= start+(end-start)/2 ;
+
+               
+                // Handle neighbors safely
+
+            long left  = (mid == 0) ? LONG_MIN : nums[mid - 1];
+            /*mid == 0 → checks if mid is the first element.
+
+If true → set left = LONG_MIN (a very small number, so nums[mid] > left is always true).
+
+If false → set left = nums[mid-1] (the actual left neighbor).*/
+
+            long right = (mid == (int)nums.size() - 1) ? LONG_MIN : nums[mid + 1];
+            /*long right = (mid == nums.size() - 1) ? LONG_MIN : nums[mid + 1];
+
+mid == nums.size() - 1 → checks if mid is the last element.
+
+If true → set right = LONG_MIN.
+
+If false → set right = nums[mid+1] (the actual right neighbor).*/
+
+
+            if(nums[mid]>left && nums[mid]>right)
+            return mid ;
+
+            else if(nums[mid]>left)
+            start=mid+1 ;
+
+            else
+            end=mid-1 ;
+          }
+
+          return start ;
+    }
+    
+};
